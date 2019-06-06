5c3c7b0821d4d44c981f62df16e83a9fcb9488e4
Alain Joseph <alainrjoseph@gmail.com>
2019-06-06 17:07:30 -0400

    creating_app

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+    
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Champ', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Buford', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Checkerburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconzilla', false);
